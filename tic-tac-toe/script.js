f7e3a24a693f5219063a1b71722c8dc346b04d36
alienfoxx <alienfoxx001@gmail.com>
2024-03-11 21:36:19 +0000

    add the basic functtion that will work on it

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,3 +1,42 @@
 const cells = document.querySelectorAll('.cell');
 const statusText = document.getElementById('statusText');
 const restartBtn = document.getElementById('restart-btn');
+var currentPlayer ="X"
+
+const gamePlay={
+    winConditions:[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+     ],
+}
+
+
+
+function initialsGame(){
+
+}
+
+ function clickedCell(){
+
+}
+
+function updateCell(){
+
+}
+
+function changePlayer(){
+
+}
+
+function checkWinner(){
+
+}
+ function restartGame(){
+
+ }
